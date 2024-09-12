3b2710ba6e16fafe042c7150b4ef454b551a3175
Hector <hcontrerascorvacho@gmail.com>
2024-09-12 14:40:08 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <>
-   
+    <h1 className="text-red-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <p className="read-the-docs">
              
   Todo
