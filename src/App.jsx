d43ea10cec3c717e3e8744abf9a03f01585759d7
Hector <hcontrerascorvacho@gmail.com>
2024-09-12 13:09:19 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+      <p className="read-the-docs">
+             
+  Todo
+
+{/* Add dynamic number */} items left
+
+All
+Active 
+Completed
+
+Clear Completed
+
+Drag and drop to reorder list
+
+      </p>
+ 
+<div class="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="nofollow noopener noreferrer">Frontend Mentor</a>. 
+  Coded by <a href="https://github.com/hectrhcc" target='_blank' rel="nofollow noopener noreferrer" >Hectrhcc</a>.
+</div>
+    </>
+  )
+}
+
+export default App
