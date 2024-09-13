4e8435a80be1ea4b3dfbd6d35d0ae23cf091f7c0
Hector <hcontrerascorvacho@gmail.com>
2024-09-13 19:51:30 -0300

    estilo version escritorio light

--- src/App.jsx
+++ src/App.jsx
@@ -15,57 +15,56 @@ function App() {
 
   return (
     <body className='bg-lightTheme-veryLightGrayishBlue'>  
-    <header  >
-    <img src={desktopdark} className='w-full relative bottom-24 xs:hidden md:inline md:bottom-48 lg:bottom-24'/>
-    <img src={mobilelight} className='w-full h-full mt-20 relative bottom-20  xs:inline md:hidden md:hidden lg:hidden md:bottom-48'/>
-    <div className='flex flex-row justify-between h-10 relative bottom-32 xs:bottom-60  '>
-    <div><h1 className="text-white text-3xl font-bold  w-full xs:ml-4 tracking-widest">
+    <header>
+    <img src={desktoplight} className='w-full relative bottom-24 mt-20 xs:hidden md:inline md:bottom-48 lg:bottom-24 '/>
+    <img src={mobilelight} className='w-full h-full mt-20 relative bottom-20  xs:inline md:hidden md:hidden lg:hidden md:bottom-48 '/>
+    <div className=' lg:w-1/3 lg:mx-auto flex flex-row xs:justify-between h-10 relative bottom-44  lg:bottom-80 xs:bottom-60 '>
+    <div><h1 className=" lg:m-0 text-white lg:text-right text-3xl font-bold  w-full xs:ml-4 tracking-widest">
     T O D O   
     </h1>
     </div>
     <div>
-    <img src={iconmoon}  className='inline pr-6'/>
+    <img src={iconmoon}  className='inline xs:pr-6 lg:p-0 '/>
     </div>
     </div>
-    <div className='bg-lightTheme-veryLightGray border w-1/3 mx-auto mb-6 p-3 rounded md:w-1/2 lg:w-1/3 xs:w-11/12   z-10 relative bottom-52 mb-8'>
+    <div className='lg:bottom-72 bg-lightTheme-veryLightGray border w-1/3 mx-auto mb-6 p-3 rounded md:w-1/2 lg:w-1/3 xs:w-11/12   z-10 relative bottom-52 mb-8'>
     <input type="radio" id="opt1" name="opt1" value="opt1"  className='ml-2' />
-    <input placeholder=' Create a new todo...' className='xs:text-xs bg-white ml-2 font-semibold'/> 
+    <input placeholder=' Create a new todo...' className='lg:text-lg xs:text-xs bg-white ml-2 font-semibold'/> 
     </div>
     </header>
 
-    <main className='z-10 relative bottom-56 '>
+    <main className='z-10 relative bottom-56 lg:bottom-72'>
     <div className='bg-lightTheme-veryLightGray border-t border-l flex items-center h-16 border-r  w-1/3 mx-auto p-3 rounded-t md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt2" name="opt2" value="opt2" className='ml-2'/>
-    <label for="option2"  className='ml-2  xs:text-xs text-gray-600 font-semibold'>Complete online JavaScript course</label>
+    <label for="option2"  className='ml-2  lg:text-lg xs:text-xs text-gray-600 font-semibold'>Complete online JavaScript course</label>
     </div>
-    <div className='bg-lightTheme-veryLightGray border-l border-t border-r flex items-center  h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
+    <div className='bg-lightTheme-veryLightGray border-l border-t border-r flex items-center h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt3" name="opt3" value="opt3"  className='ml-2 '/>
-    <label for="option3" className='ml-2 xs:text-xs text-gray-600 font-semibold'>Jag around the park 3x</label>
+    <label for="option3" className='ml-2 lg:text-lg xs:text-xs text-gray-600 font-semibold'>Jag around the park 3x</label>
     </div>
-    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center  h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
+    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt4" name="opt4" value="opt4" className='ml-2 '/>
-    <label for="option4" className='ml-2 xs:text-xs text-gray-600 font-semibold' >10 minutes meditation</label>
+    <label for="option4" className='ml-2 lg:text-lg xs:text-xs text-gray-600 font-semibold' >10 minutes meditation</label>
     </div>
-    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center  h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
+    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center h-16 w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt5" name="opt5" value="opt5" className='ml-2 '/>
-    <label for="option5" className='ml-2 xs:text-xs text-gray-600 font-semibold'>Read for 1 hour </label>
+    <label for="option5" className='ml-2 lg:text-lg xs:text-xs text-gray-600 font-semibold'>Read for 1 hour </label>
     </div>
-    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center  h-16   w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
+    <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center h-16  w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt6" name="opt6" value="opt6" className='ml-2 '/>
-    <label for="option6" className='ml-2 xs:text-xs text-gray-600 font-semibold'>Pick up groceries</label>
+    <label for="option6" className='ml-2 lg:text-lg xs:text-xs text-gray-600 font-semibold'>Pick up groceries</label>
     </div>
     <div className='bg-lightTheme-veryLightGray border-l border-r border-t flex items-center  h-16  w-1/3 mx-auto p-3 md:w-1/2 lg:w-1/3 xs:w-11/12'>
     <input type="radio" id="opt7" name="opt7" value="opt7" className='ml-2 '/>
-    <label for="option7" className='ml-2 xs:text-xs text-gray-600 font-semibold' >Complete Todo App on Frontend Mentor</label>
+    <label for="option7" className='ml-2 lg:text-lg xs:text-xs text-gray-600 font-semibold' >Complete Todo App on Frontend Mentor</label>
     </div>
-    <div className='bg-lightTheme-veryLightGray flex justify-between items-center h-10 flex-row  xs:text-xs border h-16  w-1/3 mx-auto p-3 rounded-b md:w-1/2 lg:w-1/3 xs:w-11/12 '>
+    <div className='bg-lightTheme-veryLightGray flex justify-between items-center h-10 flex-row  xs:text-xs lg:text-base border h-16  w-1/3 mx-auto p-3 rounded-b md:w-1/2 lg:w-1/3 xs:w-11/12 '>
         <div className='ml-1 text-gray-400 font-medium'>{/* Add dynamic number */} items left</div>
             <span className='xs:hidden md:inline lg:inline text-gray-500 '> All</span>
             <span className='xs:hidden md:inline lg:inline text-gray-500 '>Active </span>
             <span className='xs:hidden md:inline lg:inline text-gray-500 '>Completed</span>
         <div className='ml-1 text-gray-400 font-medium'>Clear Completed</div>
-           
-        
+               
       </div>
     <div className='bg-lightTheme-veryLightGray border mt-4  w-1/3 mx-auto text-center p-3 rounded md:w-1/2 lg:w-1/3 xs:w-11/12 lg:hidden md:hidden'>
     
