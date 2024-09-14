1766033226564777e6504b1a49211427e72791bc
Hector <hcontrerascorvacho@gmail.com>
2024-09-14 14:59:17 -0300

    array de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import './App.css'
 
 
 function App() {
+
+  let arr=['a','b','c']; //arreglo de frases del todo
+
   const [Mode, setMode] = useState(false)
 
   const handleClick = () => {
@@ -29,6 +32,16 @@ function App() {
 
   function cambioTexto(e) {
     setTexto(e.target.value)
+  }
+
+  function handleKeyPress(e){
+    if (e.keyCode === 13) {
+      handleEnter();
+    }
+  }
+
+  function  handleEnter(){
+    arr.push(texto)
   }
 
   return (
@@ -56,11 +69,24 @@ function App() {
     </header>
 
     <main className='dark:bg-darkTheme-veryDarkBlue border-none z-10 relative bottom-56 lg:bottom-72 md:bottom-72'>
-    <div className=' bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue  border-b border-lightGrayishBlue dark:border-gray-600 bg-lightTheme-veryLightGray  flex items-center h-16   w-1/3 mx-auto p-3 rounded-t md:w-1/2 lg:w-1/3 xs:w-11/12'>
-    <div className='ml-2 border rounded-full h-5 w-5 dark:border-gray-700 bg-check-gradient' ><img src={iconcheck} className='mx-auto relative top-1.5 w-1/2 '/></div>
-    <label for="option2"  className='text-darkTheme-darkGrayishBlue  dark:text-lightTheme-darkGrayishBlue ml-2  lg:text-lg xs:text-xs  font-semibold lg:ml-4  relative top-1'>Complete online JavaScript course</label>
+  
+  {
+    arr.map( (lista,indice)=>{
+    return(
+      <div className=' bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue  border-b border-lightGrayishBlue dark:border-gray-600 bg-lightTheme-veryLightGray  flex items-center h-16   w-1/3 mx-auto p-3 rounded-t md:w-1/2 lg:w-1/3 xs:w-11/12'>
+      <div className='ml-2 border rounded-full h-5 w-5 dark:border-gray-700 bg-check-gradient' ><img src={iconcheck} className='mx-auto relative top-1.5 w-1/2 '/></div>  
+      <ul className="list-none">
+    <li key={indice} className=' text-darkTheme-darkGrayishBlue  dark:text-lightTheme-darkGrayishBlue ml-2  lg:text-lg xs:text-xs  font-semibold lg:ml-4  relative top-1'>
+    {lista}</li>
+    </ul>
     </div>
+    );
+})
+}
     
+
+    
+
     <div className='bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue border-none   border-lightGrayishBlue bg-lightTheme-veryLightGray flex justify-between items-center h-10 flex-row  xs:text-xs lg:text-base h-16  w-1/3 mx-auto p-3 rounded-b md:w-1/2 lg:w-1/3 xs:w-11/12 '>
         <div className='ml-1 text-gray-400 font-medium'>{/* Add dynamic number */} items left</div>
             <span className='xs:hidden md:inline lg:inline text-gray-500 '> All</span>
