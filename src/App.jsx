ff708a087d38e02dec75cbdecff5f4922dca800a
Hector <hcontrerascorvacho@gmail.com>
2024-09-14 15:25:51 -0300

    sin array y funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -11,59 +11,59 @@ import './App.css'
 
 
 function App() {
-
-  let arr=['a','b','c']; //arreglo de frases del todo
-
-  const [Mode, setMode] = useState(false)
-
+  const [todos, setTodos] = useState([]);
+  const [mode, setMode] = useState(false);
+  const [texto, setTexto] = useState('');
+  
   const handleClick = () => {
-    setMode(!Mode)
+    setMode(!mode)
   }
 
   useEffect(() => {
-    if(Mode){
+    if(mode){
       document.documentElement.classList.add('dark')
     } else {
       document.documentElement.classList.remove('dark')
     }
-  }, [Mode])
+  }, [mode])
   
-  const [texto, setTexto] = useState('')
-
+  
   function cambioTexto(e) {
     setTexto(e.target.value)
   }
 
   function handleKeyPress(e){
-    if (e.keyCode === 13) {
+    if (e.key === 'Enter') {
       handleEnter();
     }
   }
 
   function  handleEnter(){
-    arr.push(texto)
+    if (texto.trim() !== '') {
+      setTodos([...todos, texto]);
+      setTexto('');
+    }
   }
 
   return (
     <body className='bg-gray-100 text-lightTheme-darkGrayishBlue dark:bg-darkTheme-veryDarkBlue dark:text-darkTheme-lightGrayishBlue  transition-colors duration-300'>  
     <header>
-    <img src={desktoplight} className='w-full relative bottom-24 mt-24 xs:hidden md:inline md:bottom-48 lg:bottom-24 md:mt-48 lg:mt-24 '/>
+    <img src={mode ? desktopdark : desktoplight} className='w-full relative bottom-24 mt-24 xs:hidden md:inline md:bottom-48 lg:bottom-24 md:mt-48 lg:mt-24 '  alt="background"/>
     
-    <img src={mobilelight} className='w-full h-full mt-20 relative bottom-20  xs:inline md:hidden md:hidden lg:hidden md:bottom-48 '/>
+    <img src={mode ? mobiledark : mobilelight} className='w-full h-full mt-20 relative bottom-20  xs:inline md:hidden md:hidden lg:hidden md:bottom-48 '  alt="background"/>
     <div className=' lg:w-1/3 md:w-1/2 md:mx-auto  lg:mx-auto flex flex-row xs:justify-between h-10 relative bottom-84  lg:bottom-80 md:bottom-80 xs:bottom-60 '>
     <div><h1 className=" p-0 lg:m-0 md:m-0 text-white lg:text-4xl text-3xl font-bold  w-full xs:ml-4 tracking-widest">
     T O D O   
     </h1>
     </div>
     <div>
-    <img src={iconmoon}  className='inline xs:pr-6 lg:p-0 md:p-0 ' onClick={handleClick}/>
-    
+    <img src={mode ? iconmoon : iconsun}  className='inline xs:pr-6 lg:p-0 md:p-0 ' onClick={handleClick} alt="toggle theme"/>
 
     </div>
     </div>
     <div className='bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue border-none lg:bottom-72 md:bottom-64 bg-lightTheme-veryLightGray  w-1/3 mx-auto lg:mb-6 p-3 rounded md:w-1/2 lg:w-1/3 xs:w-11/12 z-10 relative bottom-52 mb-8'>
     <div className='ml-2 border rounded-full h-5 w-5 dark:border-gray-700' ></div>
-    <input type='text' placeholder=' Create a new todo...' value={texto} onChange={cambioTexto} className='dark:bg-darkTheme-veryDarkDesaturatedBlue lg:text-lg xs:text-xs bg-white ml-2 font-semibold lg:ml-8 relative bottom-5 '/> 
+    <input type='text' placeholder=' Create a new todo...' value={texto} onChange={cambioTexto} onKeyPress={handleKeyPress} className='dark:bg-darkTheme-veryDarkDesaturatedBlue lg:text-lg xs:text-xs bg-white ml-2 font-semibold lg:ml-8 relative bottom-5 '/> 
     
     </div>
     </header>
@@ -71,22 +71,19 @@ function App() {
     <main className='dark:bg-darkTheme-veryDarkBlue border-none z-10 relative bottom-56 lg:bottom-72 md:bottom-72'>
   
   {
-    arr.map( (lista,indice)=>{
+    todos.map( (tarea,indice)=>{
     return(
       <div className=' bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue  border-b border-lightGrayishBlue dark:border-gray-600 bg-lightTheme-veryLightGray  flex items-center h-16   w-1/3 mx-auto p-3 rounded-t md:w-1/2 lg:w-1/3 xs:w-11/12'>
       <div className='ml-2 border rounded-full h-5 w-5 dark:border-gray-700 bg-check-gradient' ><img src={iconcheck} className='mx-auto relative top-1.5 w-1/2 '/></div>  
       <ul className="list-none">
     <li key={indice} className=' text-darkTheme-darkGrayishBlue  dark:text-lightTheme-darkGrayishBlue ml-2  lg:text-lg xs:text-xs  font-semibold lg:ml-4  relative top-1'>
-    {lista}</li>
+    {tarea}</li>
     </ul>
     </div>
     );
 })
 }
-    
-
-    
-
+  
     <div className='bg-white dark:bg-darkTheme-veryDarkDesaturatedBlue border-none   border-lightGrayishBlue bg-lightTheme-veryLightGray flex justify-between items-center h-10 flex-row  xs:text-xs lg:text-base h-16  w-1/3 mx-auto p-3 rounded-b md:w-1/2 lg:w-1/3 xs:w-11/12 '>
         <div className='ml-1 text-gray-400 font-medium'>{/* Add dynamic number */} items left</div>
             <span className='xs:hidden md:inline lg:inline text-gray-500 '> All</span>
